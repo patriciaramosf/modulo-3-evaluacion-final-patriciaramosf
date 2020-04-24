f90470c4844ee92ad1680260e01fcbda05382720
Patricia Ramos <patriciaramosf94@gmail.com>
2020-04-24 16:34:03 +0200

    responsive app done

--- src/components/CharacterDetails.js
+++ src/components/CharacterDetails.js
@@ -4,25 +4,27 @@ import {Link} from 'react-router-dom';
 const CharacterDetails =(props)=>{
     const {detailObj} = props;
     return(
-        <div className="CharacterDetails">
-            <Link to='/'>
-                <div className="backTo">Volver</div>
-                </Link>
-            <div className="CharacterDetails__Photo">
-                <img alt={detailObj.name} className="cdPhoto" src={detailObj.image}/>
+        <div className="CharacterDetails__container">
+            <div className="CharacterDetails">
+                <div className="CharacterDetails__Photo">
+                    <img alt={detailObj.name} className="cdPhoto" src={detailObj.image}/>
+                </div>
+                <div className="CharacterDetails__Info">
+                <h2 className="cdName">{detailObj.name}</h2>
+                    <ul className="cdInfo">
+                        <li><span className="infoTitleDetail">Species: </span>{detailObj.species}</li>
+                        <li><span className="infoTitleDetail">Gender: </span>{detailObj.gender}</li>
+                        <li><span className="infoTitleDetail">Status: </span>{detailObj.status}</li>
+                        <li><span className="infoTitleDetail">Origin: </span>{detailObj.origin.name}</li>
+                        <li><span className="infoTitleDetail">Last Location: </span>{detailObj.location.name}</li>
+                        <li><span className="infoTitleDetail">Episodes: </span>{detailObj.episode.length}</li>
+                    </ul>
+                </div>
+                <Link to='/'>
+                    <div className="backTo"><i class="fas fa-times"></i></div>
+                    </Link>
             </div>
-            <div className="CharacterDetails__Info">
-            <h2 className="cdName">{detailObj.name}</h2>
-                <ul className="chInfo">
-                    <li>Species:{detailObj.species}</li>
-                    <li>Gender:{detailObj.gender}</li>
-                    <li>Status:{detailObj.status}</li>
-                    <li>Origin:{detailObj.origin.name}</li>
-                    <li>Last Location:{detailObj.location.name}</li>
-                    <li>Episodes:{detailObj.episode.length}</li>
-                </ul>
             </div>
-        </div>
     )
 }
 export default CharacterDetails;
