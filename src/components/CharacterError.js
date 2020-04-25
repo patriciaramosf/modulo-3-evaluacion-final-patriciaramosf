5ead30cc44e30331148ca0c114be04ea9e137bb1
Patricia Ramos <patriciaramosf94@gmail.com>
2020-04-25 18:39:17 +0200

    change error from the error page

--- src/components/CharacterError.js
+++ src/components/CharacterError.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import icon from '../images/mistake.png'
+import icon from '../images/mistake.png' 
 const CharacterError =(props)=>{
     return(
         <div className="CharacterError">
-            <div className="ceGeneral">
+           <div className="ceGeneral">
+           <div className="blue">
+                <p className="ceText">{`ERROR, ${props.inputValue} not found!`}</p>
+            </div> 
             <img alt="rick error" className="ceImg" src={icon}></img>
-            <p className="ceText">{`ERROR, ${props.inputValue} doesn´t exist!`}</p>
-
             </div>
         </div>
     )
