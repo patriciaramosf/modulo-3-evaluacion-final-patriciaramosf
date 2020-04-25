bddef9eaff47cae6d2545ef30ce6541be61a50d0
Patricia Ramos <patriciaramosf94@gmail.com>
2020-04-25 15:40:02 +0200

    second final project

---
+++ docs/build/precache-manifest.32464f472752b9ead27dc0a7562cc64a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "756cab3413e81167fbcdb9886f8b8e3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8109e00df9c8e496aeed",
+    "url": "/static/css/main.8a60d3b4.chunk.css"
+  },
+  {
+    "revision": "42bca2f544bc82415f14",
+    "url": "/static/js/2.10b92ad3.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.10b92ad3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8109e00df9c8e496aeed",
+    "url": "/static/js/main.dec51417.chunk.js"
+  },
+  {
+    "revision": "483e671ea2c1d9fa3837",
+    "url": "/static/js/runtime-main.5f283f6a.js"
+  },
+  {
+    "revision": "416ec287c4b08db1d72b0e38a6592a3c",
+    "url": "/static/media/mistake.416ec287.png"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "/static/media/rick_logo.75509f2e.png"
+  }
+]);
